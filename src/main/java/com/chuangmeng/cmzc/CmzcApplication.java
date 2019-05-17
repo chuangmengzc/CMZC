f4c28e77a78e5bea525294671c41fb666e305665
mingmingzou <1355295032@qq.com>
2019-05-17 20:25:45 +0800

    zmm第一次提交

--- src/main/java/com/chuangmeng/cmzc/CmzcApplication.java
+++ src/main/java/com/chuangmeng/cmzc/CmzcApplication.java
@@ -1,9 +1,11 @@
 package com.chuangmeng.cmzc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.chuangmeng.cmzc.*.dao")
 public class CmzcApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/chuangmeng/cmzc/commons/vo/PackageImgsVo.java
@@ -0,0 +1,51 @@
+package com.chuangmeng.cmzc.commons.vo;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class PackageImgsVo {
+    private MultipartFile packagePic1;
+    private MultipartFile packagePic2;
+    private MultipartFile packagePic3;
+    private MultipartFile packagePic4;
+    private MultipartFile packagePic5;
+
+    public MultipartFile getPackagePic1() {
+        return packagePic1;
+    }
+
+    public void setPackagePic1(MultipartFile packagePic1) {
+        this.packagePic1 = packagePic1;
+    }
+
+    public MultipartFile getPackagePic2() {
+        return packagePic2;
+    }
+
+    public void setPackagePic2(MultipartFile packagePic2) {
+        this.packagePic2 = packagePic2;
+    }
+
+    public MultipartFile getPackagePic3() {
+        return packagePic3;
+    }
+
+    public void setPackagePic3(MultipartFile packagePic3) {
+        this.packagePic3 = packagePic3;
+    }
+
+    public MultipartFile getPackagePic4() {
+        return packagePic4;
+    }
+
+    public void setPackagePic4(MultipartFile packagePic4) {
+        this.packagePic4 = packagePic4;
+    }
+
+    public MultipartFile getPackagePic5() {
+        return packagePic5;
+    }
+
+    public void setPackagePic5(MultipartFile packagePic5) {
+        this.packagePic5 = packagePic5;
+    }
+}
